c98361b373347c62bd53284bbedbf7174edee6a1
jai <jaimendezofficial@gmail.com>
2021-09-21 14:40:40 +0000

    part2

--- act2.py
+++ act2.py
@@ -5,3 +5,11 @@ finals = int(input("Enter Finals Grade: "))
 
 avg = (prelims + midterm + semis + finals)/4
 
+passing = 75
+
+if avg >= passing:
+    print("You passed!")
+
+else:
+    print("You Failed")
+
