ad843238dd5b9d94b7a1e80422ece10456c52f53
jai <jaimendezofficial@gmail.com>
2021-09-21 14:55:50 +0000

    part3

--- act2.py
+++ act2.py
@@ -7,9 +7,50 @@ avg = (prelims + midterm + semis + finals)/4
 
 passing = 75
 
-if avg >= passing:
-    print("You passed!")
+if avg >= passing :
+
+    if avg >= 98:
+        print(avg)
+        print("Grade : A")
+        print("You Passed")
+
+    elif avg >= 90 and avg <99:
+        print(avg)
+        print("Grade : B")
+        print("You Passed")
+
+    elif avg >= 90 and avg <99:
+        print(avg)
+        print("Grade : B")
+        print("You Passed")
+
+    elif avg >= 80 and avg <90:
+        print(avg)
+        print("Grade : C")
+        print("You Passed")
+
+    elif avg >= 75 and avg <80:
+        print(avg)
+        print("Grade : D")
+        print("You Passed")
 
 else:
-    print("You Failed")
+
+    elif avg >= 71 and avg <75:
+        print(avg)
+        print("Grade : D")
+        print("You're a Failure")
+
+    elif avg >= 61 and avg <71:
+        print(avg)
+        print("Grade : E")
+        print("You're a Failure")  
+
+    elif avg <= 60:
+        print(avg)
+        print("Grade : F")
+        print("You're a Failure")      
+
+
+    
 
