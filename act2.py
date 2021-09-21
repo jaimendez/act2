b155372f36af9817732ac989f966a3e68bc7fabf
jai <jaimendezofficial@gmail.com>
2021-09-21 14:23:16 +0000

    part 1

---
+++ act2.py
@@ -0,0 +1,7 @@
+prelims = int(input("Enter Prelims grade: "))
+midterm = int(input("Enter Midterm grade: "))
+semis = int(input("Enter Semi Finals Grade: "))
+finals = int(input("Enter Finals Grade: "))
+
+avg = (prelims + midterm + semis + finals)/4
+
